eb090bbf66401ece9de070e6a80ce795541bd71a
Kristineberg <412258712@qq.com>
2023-07-11 15:01:52 +0800

    refactor: remove JsonSerialize annotation

--- lbs-client-core/src/main/java/com/starimmortal/lbs/request/RectangleSearchRequest.java
+++ lbs-client-core/src/main/java/com/starimmortal/lbs/request/RectangleSearchRequest.java
@@ -1,7 +1,5 @@
 package com.starimmortal.lbs.request;
 
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.starimmortal.lbs.serializer.LbsRequestSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -24,7 +22,6 @@ public class RectangleSearchRequest extends PlaceSearchRequest {
 	/**
 	 * 格式：boundary=rectangle(lat,lng,lat,lng) 子参数：由矩形左下角（西南）和右上角（东北）两对经纬度组成，格式顺序为纬度在前，经度在后
 	 */
-	@JsonSerialize(using = LbsRequestSerializer.class)
 	private RectangleBoundaryRequest boundary;
 
 }
